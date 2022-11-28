a6013522b6a8e8f1892cc2ae3fb666128a05fcd7
David Bradburn <david.bradburn2@gmail.com>
2022-11-28 23:44:30 +0000

    Update 13_1.py

--- Day 13/1/13_1.py
+++ Day 13/1/13_1.py
@@ -25,27 +25,46 @@ raw_len_y = len(test)
 raw_len_x = len(test[0])
 
 track_map = np.empty((raw_len_y, raw_len_x), dtype=str)
-train_map = np.zeros_like(track_map, dtype=str)
+train_map = np.empty_like(track_map, dtype='O')
+
+class train():
+
+	def __init__(self, st_direction) -> None:
+
+		self.current_direction = st_direction
+		self.next_direction_arr = ['l', 's', 'r']
+		self.next_direction_index = 0
+
+		self.next_corssing_direction = self.next_direction_arr[self.next_direction_index]
+
+	def update_next_crossing(self):
+		self.next_direction_index += 1
+		if self.next_direction_index > 2:
+			self.next_direction_index = 0
+
+		self.next_corssing_direction = self.next_direction_arr[self.next_direction_index]
+
+
 for row_index, row in enumerate(test):
 	assert len(row) == raw_len_x
 	for column_index, value in enumerate(row):
 		match value:
 			case '<':
-				train_map[row_index][column_index] = 'l'
+				train_map[row_index][column_index] = train('l')
 				track_map[row_index][column_index] = '-'
 				
 			case '>':
 				
-				train_map[row_index][column_index] = 'r'
+				train_map[row_index][column_index] = train('r')
 				track_map[row_index][column_index] = '-'
 				
 			case '^':
 				
-				train_map[row_index][column_index] = 'u'
+				train_map[row_index][column_index] = train('u')
 				track_map[row_index][column_index] = '|'
 				
 			case 'v':
-				train_map[row_index][column_index] = 'd'
+				train_map[row_index][column_index] = train('d')
 				track_map[row_index][column_index] = '|'
 				
 			case _:
@@ -56,10 +75,16 @@ for row_index, row in enumerate(test):
 def update_direction(direction, location):
 	match direction:
 		case "l":
-			next_track = track_map[colum_index-1][row_index]
+			next_track_direction = track_map[colum_index-1][row_index]
 		
 		case "r":
-			next_track = track_map[colum_index-1][row_index]
+			next_track_direction = track_map[colum_index+1][row_index]
+		
+		case "u":
+			next_track_direction = track_map[colum_index][row_index-1]
+
+		case "d":
+			next_track_direction = track_map[colum_index][row_index+1]
 
 	return new_direction
 
@@ -68,7 +93,7 @@ for row_index, row in enumerate(train_map): # one tick
 	for colum_index, value in enumerate(row):
 		match value:
 			case '':
-				pass:
+				pass
 			
 			case 'l':
 
