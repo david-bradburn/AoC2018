b53dbdb657814378ba58c0e303f66755198a6051
David Bradburn <42315620+david-bradburn@users.noreply.github.com>
2022-12-08 16:26:59 +0000

    Day 3 update
    input processing and main class

--- Day 3/1/3_1.py
+++ Day 3/1/3_1.py
@@ -2,6 +2,8 @@
 ###### https://adventofcode.com/2018/day/3 ######################
 #################################################################
 
+import re
+
 file = "test.txt"
 
 DAY_NO = "3"
@@ -10,4 +12,22 @@ PART = "1"
 file_path_base = "Day " + DAY_NO + "/misc/"
 
 with open(file_path_base + file, "r") as fd:
-	pass
+	rawInput = fd.readlines()
+
+cleanerInput = []
+for line in rawInput:
+	# print(line)
+	temp = line.strip('\n')
+	print(temp)
+	temp = re.split('#| @ |,|: |x', temp)
+	cleanerInput.append([temp[1], temp[2], temp[3], temp[4], temp[5]])
+	# print(temp)
+print(cleanerInput)
+
+class Fabric():
+
+	def __init__(self, input) -> None:
+		self.claims = input
+
+
+p1 = Fabric(cleanerInput)
